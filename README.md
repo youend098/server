58e9614c7fa86108c999e83879120ceef2b6727f
unknown <harythsavage@gmail.com>
2025-03-23 18:30:41 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+### LOGIN USER ROOT
+<pre><code>sudo su</code></pre>
+
+### GANTI PASWORD ROOT
+<pre><code>sudo passwd root</code></pre>
+
+### SETTING PERMISSION LOGIN ROOT
+<pre><code>nano /etc/ssh/sshd_config</code></pre>
+
+### INSTALL SCRIPT 
+<pre><code>apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/youend098/server/main/setup.sh && chmod +x setup.sh && ./setup.sh
+</code></pre>
+
+### UPDATE SCRIPT
+<pre><code>wget https://raw.githubusercontent.com/youend098/server/main/update.sh && chmod +x update.sh && ./update.sh</code></pre>
+
+### SUPPORT OS RECOMENDED
+- UBUNTU 20
+- DEBIAN 10
+
+### INFO PORT
+- TROJAN WS 443
+- TROJAN GRPC 443
+- SHADOWSOCKS WS 443
+- SHADOWSOCKS GRPC 443
+- VLESS WS 443
+- VLESS GRPC 443
+- VLESS NONTLS 80
+- VMESS WS 443
+- VMESS GRPC 443
+- VMESS NONTLS 80
+- SSH WS / TLS 443
+- SSH NON TLS 8880
+- OVPN SSL/TCP 1194
+- SLOWDNS 5300
+
---
+++ limit/cf.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+MYIP=$(wget -qO- icanhazip.com);
+apt install jq curl -y
+read -p "Masukan Domain (contoh : youend098)" domen
+DOMAIN=karyna.my.id
+sub=${domen}
+#(</dev/urandom tr -dc a-z0-9 | head -c5)
+dns=${sub}.karyna.my.id
+CF_ID=harythsavage@gmail.com
+CF_KEY=6702cb10644a1bfe0394000bc7fb6ef7277e6
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${dns}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "$dns" > /root/domain
+echo "$dns" > /root/scdomain
+echo "$dns" > /etc/xray/domain
+echo "$dns" > /etc/v2ray/domain
+echo "$dns" > /etc/xray/scdomain
+echo "IP=$dns" > /var/lib/kyt/ipvps.conf
+cd
